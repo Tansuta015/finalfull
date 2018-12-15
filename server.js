4132aadd25ae493eac60932489e8872675c49d28
tansuta <gilf_512@hotmail.com>
2018-12-15 14:50:06 +0700

    test

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var db = require('./database')
+var cors = require('cors');
+var bodyParser = require('body-parser');
+app.use(cors());
+app.use(bodyParser.json()); //บรรทัดนี้สำคัญมาก
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+//Add routing
+app.get('/', function (req, res) {
+    res.send('Express is running');
+});
+var output = {
+    status: 'success',
+    message: 'REST API is working'
+}
+app.get('/api/json', function (req, res) {
+    res.status(500).json(output);
+
+
+});
+//customer
+app.get('/api/customers/', db.getAllCustomers);
+
+
+
+var port = process.env.PORT || 8080; //เผื่อขึ้นheroku
+app.listen(port, function () {
+    console.log('App is running on http://localhost:' + port);
+});
